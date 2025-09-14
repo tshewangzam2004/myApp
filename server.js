f2a59c8f2fbc53d9e55857822a9224476c29c229
unknown <https://github.com/tshewangzam2004/myApp.git>
2025-09-14 22:56:31 +0530

    Updated server.js with new changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 8090;
+const port = process.env.port || 8060;
 
 app.get("/",(req, res) => {
   res.send("Manju Rai! Hello, World!");
